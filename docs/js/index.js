1bc77fa19fbb093aea15197ef67dc1e0a09d9fdc
MattieChu <MattieChu@users.noreply.github.com>
2020-12-10 16:09:21 +0300

    backup

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,21 +1,34 @@
 function result_pars(data) {
-  let result_div = document.getElementById("result");
 
-  let name_div = document.createElement("div");
-  name_div.className = "name";
-  name_div.innerHTML = data["species"]["name"];
-
-  let image = document.createElement("img");
-  image.className = "image";
-  image.src = data["sprites"]["front_default"];
-
-  result_div.append(name_div, image);
-  console.log(name);
+for(let i=0;i<data.length;i++){
+  fetch_for_image(data[i]['name'],data[i]['url'])
+  }
+  
 }
 
-function fetch_request() {
+function createDOM(name,data){
+  let result_div = document.getElementById("result");
+  let name_div;  let image;
+  name_div = document.createElement("div");
+  name_div.className = "name";
+  name_div.innerHTML = name;
+ 
+  image = document.createElement("img");
+  image.className = "image";
+  image.src = data["sprites"]["front_default"];
+ 
+  result_div.append(name_div, image);
+}
+
+function fetch_for_image(name,url){
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {console.log(data);createDOM(name,data)});
+}
+
+function fetch_request(url) {
   let id = document.getElementById("id").value;
-  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  fetch(`https://pokeapi.co/api/v2/pokemon?limit=40&offset=${id}`)
     .then((response) => response.json())
-    .then((data) => result_pars(data));
+    .then((data) => {console.log(data);result_pars(data['results'])});
 }
