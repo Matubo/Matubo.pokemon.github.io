016852357d1c9329cd4b1a0c6c513a3bd5c2dddc
Matthew <Matbei14@yandex.ru>
2020-12-10 18:36:44 +0300

    undefined

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,34 +1,56 @@
-function result_pars(data) {
-
-for(let i=0;i<data.length;i++){
-  fetch_for_image(data[i]['name'],data[i]['url'])
+function fetch_request(obj) {
+  switch (obj.request) {
+    case "GETONEID": {
+      if (obj.url) {
+        fetch(obj.url)
+          .then((response) => response.json())
+          .then((data) => {
+            createDOM(data);
+          });
+        console.log(true);
+        return true;
+      } else {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${obj.id}`)
+          .then((response) => response.json())
+          .then((data) => {
+            createDOM(data);
+          });
+        return true;
+      }
+    }
+    case "GETSOMEID": {
+      fetch(`https://pokeapi.co/api/v2/pokemon?limit=40&offset=${obj.id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          for (let i = 0; i < data["results"].length; i++) {
+            fetch_request({
+              request: "GETONEID",
+              url: data["results"][i]["url"],
+            });
+          }
+        });
+    }
   }
-  
 }
 
-function createDOM(name,data){
+function createDOM(data) {
   let result_div = document.getElementById("result");
-  let name_div;  let image;
+  let name_div;
+  let image;
   name_div = document.createElement("div");
   name_div.className = "name";
-  name_div.innerHTML = name;
- 
+  name_div.innerHTML = data["species"]["name"];
+
   image = document.createElement("img");
   image.className = "image";
   image.src = data["sprites"]["front_default"];
- 
+
   result_div.append(name_div, image);
 }
 
-function fetch_for_image(name,url){
-  fetch(url)
-  .then((response) => response.json())
-  .then((data) => {console.log(data);createDOM(name,data)});
-}
-
-function fetch_request(url) {
+function request() {
   let id = document.getElementById("id").value;
-  fetch(`https://pokeapi.co/api/v2/pokemon?limit=40&offset=${id}`)
-    .then((response) => response.json())
-    .then((data) => {console.log(data);result_pars(data['results'])});
+  fetch_request({ request: "GETONEID", id: id });
+  fetch_request({ request: "GETSOMEID", id: id });
 }
