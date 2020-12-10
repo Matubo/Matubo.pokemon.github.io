ec30452f936ee45e6e31309e383406123832a32c
Matthew <Matbei14@yandex.ru>
2020-12-10 18:47:37 +0300

    undefined

--- docs/js/index.js
+++ docs/js/index.js
@@ -36,17 +36,22 @@ function fetch_request(obj) {
 
 function createDOM(data) {
   let result_div = document.getElementById("result");
+  let id;
   let name_div;
   let image;
   name_div = document.createElement("div");
   name_div.className = "name";
-  name_div.innerHTML = data["species"]["name"];
+  name_div.innerHTML = `name: ${data["species"]["name"]}`;
+
+  id = document.createElement("div");
+  id.className = "id";
+  id.innerHTML = `id: ${data["id"]}`;
 
   image = document.createElement("img");
   image.className = "image";
   image.src = data["sprites"]["front_default"];
 
-  result_div.append(name_div, image);
+  result_div.append(id, name_div, image);
 }
 
 function request() {
