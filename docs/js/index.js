14e68c5c1e2287989aeedd68668405b66fd5cfaa
Matthew <Matbei14@yandex.ru>
2020-12-12 20:32:14 +0300

    global refactoring. Working version

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,37 +1,100 @@
-function fetch_request(obj) {
+document.addEventListener("DOMContentLoaded", test());
+
+function test() {
+  let select_elem = document.getElementById("form_select");
+  select_elem.onchange = function () {
+    if (select_elem.value == "option1") {
+      change_DOM({ type: "first_option" });
+    }
+    if (select_elem.value == "option2") {
+      change_DOM({ type: "second_option" });
+    }
+  };
+}
+
+function change_DOM(option) {
+  if (option.type == "first_option") {
+    document.getElementById("count").setAttribute("disabled", "disabled");
+    document.getElementById("input_about").innerHTML =
+      "Номер от 0, элемент вне диапазона вернет ошибку.";
+  }
+  if (option.type == "second_option") {
+    document.getElementById("count").removeAttribute("disabled");
+    document.getElementById("input_about").innerHTML =
+      "Номер от 0 и диапазон, элемент вне диапазона вернет ошибку.";
+  }
+}
+
+function clear_result_box() {
+  let element = document.getElementById("result");
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+async function request() {
+  clear_result_box();
+
+  let option = document.getElementById("form_select").value;
+  let id = document.getElementById("id").value;
+  console.log(id);
+  let count = document.getElementById("count").value;
+
+  if (option == "option1") {
+    let result = await fetch_request({
+      request: "GETONEID",
+      id: id,
+      count: count,
+    });
+    createDOM(result);
+  }
+
+  if (option == "option2") {
+    let results = await fetch_request({
+      request: "GETSOMEID",
+      id: id,
+      count: count,
+    });
+    results = results["results"];
+    let i = 0;
+    while (i < results.length) {
+      let result = await fetch_request({
+        request: "GETBYURL",
+        url: results[i]["url"],
+      });
+      createDOM(result);
+      i++;
+    }
+  }
+}
+
+async function fetch_request(obj) {
   switch (obj.request) {
     case "GETONEID": {
-      if (obj.url) {
-        fetch(obj.url)
-          .then((response) => response.json())
-          .then((data) => {
-            createDOM(data);
-          });
-        console.log(true);
-        return true;
-      } else {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${obj.id}`)
-          .then((response) => response.json())
-          .then((data) => {
-            createDOM(data);
-          });
-        return true;
-      }
-    }
-    case "GETSOMEID": {
-      fetch(`https://pokeapi.co/api/v2/pokemon?limit=40&offset=${obj.id}`)
+      let result = await fetch(`https://pokeapi.co/api/v2/pokemon/${obj.id}`)
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
-          let i = 0;
-          while (i != data["results"].length) {
-            fetch_request({
-              request: "GETONEID",
-              url: data["results"][i]["url"],
-            });
-            i++;
-          }
+          return data;
         });
+      return result;
+    }
+    case "GETSOMEID": {
+      let result = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?limit=${obj.count}&offset=${obj.id}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          return data;
+        });
+      return result;
+    }
+    case "GETBYURL": {
+      let result = await fetch(obj.url)
+        .then((response) => response.json())
+        .then((data) => {
+          return data;
+        });
+      return result;
     }
   }
 }
@@ -59,10 +122,4 @@ function createDOM(data) {
   elem.append(image, id, name_div);
 
   result_div.append(elem);
-}
-
-function request() {
-  let id = document.getElementById("id").value;
-  fetch_request({ request: "GETONEID", id: id });
-  fetch_request({ request: "GETSOMEID", id: id });
 }
